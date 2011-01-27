a04a21b5a67b258340f8624b7fe6ff535eabed5e
Crhan <crhan123@gmail.com>
2011-01-27 15:34:01 +0800

    initial commit:wq

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>陈若涵的数据库课程设计</title>
+</head>
+<body>
+<h1><a href="index.php">学生成绩管理<sub title="Crhan"><small>By Crhan</small></sub></a></h1>
+<h2>《数据库原理及应用》短学期课程设计(大型实验)</h2>
+<div>
+<h3>请点击下面的按钮完成查询</h3>
+<p class="clearfix">
+<form name="form1" method="post" action="" class="formButton">
+<input name="" value="a" type="submit" />
+<input name="a" type="hidden" value="a" />
+</form>
+<form name="form2" method="post" action="" class="formButton">
+<input name="" value="b" type="submit" />
+<input name="a" type="hidden" value="b" />
+</form>
+<form name="form3" method="post" action="" class="formButton">
+<input name="" value="c" type="submit" />
+<input name="a" type="hidden" value="b" />
+</form>
+<form name="form4" method="post" action="" class="formButton">
+<input name="" value="d" type="submit" />
+<input name="a" type="hidden" value="b" />
+</form>
+<form name="form5" method="post" action="" class="formButton">
+<input name="" value="e" type="submit" />
+<input name="a" type="hidden" value="b" />
+</form>
+
+<?php
+require("query.php");
+?>
+</p>
+</div>
+</body>
+</html>
---
+++ query.php
@@ -0,0 +1,12 @@
+<?php
+switch($_REQUEST["a"]){
+case "a":
+    echo "<p>haha</p>";
+    break;
+case "b":
+    echo"<p>hoho</p>";
+    break;
+default:
+    echo "<p>lala</p>";
+}
+?>
